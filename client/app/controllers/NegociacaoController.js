696afa63ec43a74cfe8dbfe0e9a0e061b4bfef79
epitacio <epitacio.sobrinho00@hotmail.com>
2023-12-01 08:34:18 -0300

    criado novo modelo para listagem de multiplas negociacoes e abstração de instancia de Negociacao em metodo pricado no controller de negociacao
    removido codigo desnecessário do metodo adiciona do controller NegociacaoController.js e modificado o metodo paraArray da classe Negociacoes.js para retornar uma copia do array original

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -6,19 +6,30 @@ class NegociacaoController {
     this._inputData = $("#data");
     this._inputQuantidade = $("#quantidade");
     this._inputValor = $("#valor");
+
+    this._negociacoes = new Negociacoes();
+  }
+
+  _limpaFormulario() {
+    this._inputData.value = "";
+    this._inputQuantidade = 1;
+    this._inputValor.value = 0.0;
+    this._inputData.focus();
+  }
+
+  _criaNegociacao() {
+    return new Negociacao(
+      DateConverter.paraData(this._inputData.value),
+      parseInt(this._inputQuantidade.value),
+      parseFloat(this._inputValor.value)
+    );
   }
 
   adiciona(event) {
     event.preventDefault();
 
-    let data = DateConverter.paraData(this._inputData.value);
+    this._negociacoes.adiciona(this._criaNegociacao());
 
-    let negociacao = new Negociacao(
-      data,
-      parseInt(this._inputQuantidade.value),
-      parseFloat(this._inputValor.value)
-    );
-
-    console.log(negociacao);
+    this._limpaFormulario();
   }
 }
